6db38c61eaf16a80b3f7d2b4bf08a258255afce3
harishrgithub1 <140145289+harishrgithub1@users.noreply.github.com>
2023-07-21 12:12:40 +0530

    Update streamlit_app.py
    Corrected the code error on first breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.title ('Breakfast Menu')
 
-streamlit.title ('Omega 3 & Blueberry Oatmeal)
+streamlit.title ('Omega 3 & Blueberry Oatmeal')
